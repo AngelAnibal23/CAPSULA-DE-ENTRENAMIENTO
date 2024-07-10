05f33c130cbb3e239e5912261ee9b441b7ff6e17
Guts <aamamanica@unjbg.edu.pe>
2024-07-10 11:11:33 -0500

    SU8biendo ejecicio ps

---
+++ Ejercicio2ATS.cpp
@@ -0,0 +1,22 @@
+/*
+Ejercicio 2 ATS: 
+Realiza un programa que defina una matriz de 3x3 y escriba un ciclo 
+para que muestre la diagonal principal de la matriz. 
+*/
+
+#include <iostream>
+using namespace std; 
+int main(){
+	int matriz[3][3]={{1,2,3},{4,5,6}, {7,8,9}};  
+	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			if(i==j){
+				cout<<matriz[i][j]; 
+			}
+		}
+	}
+	
+	
+	return 0; 
+}
