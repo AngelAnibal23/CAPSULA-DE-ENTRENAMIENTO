d48f6dedc7dbbcac4254e2b32ba9c0cff5fce215
Guts <aamamanica@unjbg.edu.pe>
2024-09-11 08:05:03 -0500

    Agregando funciuonalidada para potencias negativas

--- RECURSION8.cpp
+++ RECURSION8.cpp
@@ -1,24 +1,51 @@
 #include <iostream> 
 using namespace std; 
 
-int potencia(int a, int b){
+double potencia(int a, int b){
 	if(b==1){
 		return a; 
 	}
-	else{                   
-		return a * potencia(a, b-1); 
+	else if(b==0){
+		return 1; 
 	}
-	
-	
+	else{                   
+		if(b>=0){
+			return a * potencia(a, b-1); 
+		}
+		else{
+			return a * potencia(a, b+1);
+		}
+	}	
 }
 
+/*
+double potenciaNegativa(int a, int b){
+	if(b==1){
+		return a; 
+	}
+	else if(b==0){
+		return 1; 
+	}
+	else{                   
+		return a * potencia(a, b+1); 
+	}	
+}
+*/
+
 int main(){
-	int a, b; 
+	double a, b; 
 	
 	cout<<"Digite el numero  y la potencia: "; 
 	cin>>a>>b; 
 	
-	cout<<"\nLa potencia del numero que quiere obtener es: "<<potencia(a,b); 
+	if(b>=0){
+		cout<<"\nLa potencia del numero que quiere obtener es: "<<potencia(a,b); 
+	}
+	
+	else{
+		cout<<"\nLa potencia del numero que quiere obtener es: "<<potencia(a,b); 
+	}
+	
 	
 	return 0; 
 }
