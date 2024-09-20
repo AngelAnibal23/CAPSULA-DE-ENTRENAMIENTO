ee6f5108e64dd1435963d0adc38963cceba097d4
Guts <aamamanica@unjbg.edu.pe>
2024-09-20 09:54:51 -0500

    forma de barraza inserccion

--- inserccionOR.cpp
+++ inserccionOR.cpp
@@ -5,14 +5,14 @@ int main(){
 	int a[]={1,3,4,21,34,5,2,4,7,8}; 
 	int k, aux;
 	
-	for(int i=0; i<10; i++){
+	for(int i=1; i<10; i++){
 		aux = a[i]; 
-		k = i;  
-		while((k>0) && (a[k-1]>aux)){
-			a[k] = a[k-1]; 
+		k = i-1;  
+		while((k>0) && (aux<a[k])){
+			a[k+1] = a[k]; 
 			k--; 
 		}
-		a[k] = aux; 
+		a[k+1] = aux; 
 	}
 	
 	for(int i=0; i<10; i++){
