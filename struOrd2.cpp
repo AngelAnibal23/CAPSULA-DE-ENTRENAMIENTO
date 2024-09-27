c73444f650761863f15f0b545b2d5ae42219a53c
Guts <aamamanica@unjbg.edu.pe>
2024-09-27 09:41:14 -0500

    Ejercicio2REGISTRO

--- struOrd2.cpp
+++ struOrd2.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 struct Articulo{
 	string codigo; 
-	float precio; 
-	int cantidad; 
+	int precio; 
+	int cantidad;
+	int montoInve;  
 };
 
 void llenarRegistro(int n, Articulo art[]){
 	for(int i=0; i<n; i++){
-		cout<<"\nPolinomio "<<i+1<<" :"<<endl; 
-		cout<<"Digite el codigo del articulo: "; 
+		cout<<"\nArticulo"<<i+1<<" :"<<endl; 
+		cout<<"Digite el codigo del articulo: ";
 		cin.ignore();
 		getline(cin, art[i].codigo); 
 		cout<<"Digite el precio del articulo: "; 
@@ -19,6 +20,9 @@ void llenarRegistro(int n, Articulo art[]){
 		cout<<"Digite la cantidad de el articulo: "; 
 		cin>>art[i].cantidad;  
 		cout<<"___________________________\n";
+		
+		art[i].montoInve = art[i].precio * art[i].cantidad;
+		
 	}
 }
 
@@ -27,7 +31,8 @@ void mostrarRegistro(int n, Articulo a[]){
 		cout<<"\nArticulo "<<i+1<<": "; 
 		cout<<"Codigo: "<<a[i].codigo<<endl;
 		cout<<"Precio: "<<a[i].precio<<endl; 
-		cout<<"Cantidad: "<<a[i].cantidad<<endl; 
+		cout<<"Cantidad: "<<a[i].cantidad<<endl;
+		cout<<"Monto de inversion: "<<a[i].montoInve<<endl;  
 		cout<<"___________________________________\n";
 	}  
 	cout<<endl; 
@@ -38,12 +43,27 @@ int main(){
 	
 	cout<<"Digite la cantidad de elementos de la estructura: "; 
 	cin>>n; 
-	
-	int montoInve[n]; 
+	 
 	Articulo art[n]; 
 	
 	llenarRegistro(n, art);
+	
+	Articulo aux; 
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-1-i; j++){
+			if(art[j].montoInve < art[j+1].montoInve){
+				aux = art[j]; 
+				art[j] = art[j+1]; 
+				art[j+1] = aux; 
+			}
+		}
+	}
+	
+	cout<<"\nOrden descendente segun monto de inversion: "<<endl; 
 	mostrarRegistro(n, art);
+	
+	cout<<endl; 
 	
 	return 0; 
 }
