1fa385f735ff6dedf1d7f3b69e6b4bac92cb3cd3
Guts <aamamanica@unjbg.edu.pe>
2024-09-26 20:36:04 -0500

    gaaaaa

--- struOrd2.cpp
+++ struOrd2.cpp
@@ -8,6 +8,31 @@ struct Articulo{
 	int cantidad; 
 };
 
+void llenarRegistro(int n, Articulo art[]){
+	for(int i=0; i<n; i++){
+		cout<<"\nPolinomio "<<i+1<<" :"<<endl; 
+		cout<<"Digite el codigo del articulo: "; 
+		cin.ignore();
+		getline(cin, art[i].codigo); 
+		cout<<"Digite el precio del articulo: "; 
+		cin>>art[i].precio;
+		cout<<"Digite la cantidad de el articulo: "; 
+		cin>>art[i].cantidad;  
+		cout<<"___________________________\n";
+	}
+}
+
+void mostrarRegistro(int n, Articulo a[]){
+	for(int i=0; i<n; i++){
+		cout<<"\nArticulo "<<i+1<<": "; 
+		cout<<"Codigo: "<<a[i].codigo<<endl;
+		cout<<"Precio: "<<a[i].precio<<endl; 
+		cout<<"Cantidad: "<<a[i].cantidad<<endl; 
+		cout<<"___________________________________\n";
+	}  
+	cout<<endl; 
+}
+
 int main(){
 	int n; 
 	
@@ -17,7 +42,8 @@ int main(){
 	int montoInve[n]; 
 	Articulo art[n]; 
 	
-	
+	llenarRegistro(n, art);
+	mostrarRegistro(n, art);
 	
 	return 0; 
 }
