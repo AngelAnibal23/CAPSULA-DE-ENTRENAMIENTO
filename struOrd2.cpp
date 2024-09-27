b268cc2bb23acf0713bf1fc0ea19ed48b419ba5a
Guts <aamamanica@unjbg.edu.pe>
2024-09-26 19:48:20 -0500

    registros 2

---
+++ struOrd2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+struct Articulo{
+	string codigo; 
+	float precio; 
+	int cantidad; 
+};
+
+int main(){
+	int n; 
+	
+	cout<<"Digite la cantidad de elementos de la estructura: "; 
+	cin>>n; 
+	
+	int montoInve[n]; 
+	Articulo art[n]; 
+	
+	
+	
+	return 0; 
+}
