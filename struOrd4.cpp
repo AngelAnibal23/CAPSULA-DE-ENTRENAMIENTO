c712105257215992c47f633605d013515f6b2df6
Guts <aamamanica@unjbg.edu.pe>
2024-09-28 11:26:44 -0500

    ejercicio4

---
+++ struOrd4.cpp
@@ -0,0 +1,28 @@
+/*
+3. Se tienen los siguientes atributos de personal: DNI, nombres, día, mes y año de nacimiento. 
+Se pide ordenar por fecha de nacimiento. Sugerencia: Use dos registros (uno para personal y otro para fecha) 
+y dos funciones (una para la ordenación por 
+inserción directa y otra para la comparación de dos fechas). 
+*/
+
+#include <iostream> 
+#include <string>
+using namespace std; 
+
+struct Personal{
+	string dni; 
+	string nombres;  
+};
+
+struct Fecha{
+	int dia; 
+	int mes; 
+	int anho; 	
+};
+
+int main(){
+	
+	
+	
+	return 0; 
+}
