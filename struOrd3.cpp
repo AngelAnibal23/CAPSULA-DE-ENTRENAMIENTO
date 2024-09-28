29500430118851bdce78f66c80225619dce115c4
Guts <aamamanica@unjbg.edu.pe>
2024-09-28 11:26:29 -0500

    ejercicio 3 registros

---
+++ struOrd3.cpp
@@ -0,0 +1,18 @@
+/*
+3. Se tienen los siguientes atributos de personal: DNI, nombres, día, mes y año de nacimiento. 
+Se pide ordenar por fecha de nacimiento. 
+Sugerencia: Use dos registros (uno para personal y otro para fecha) y dos funciones
+(una para la ordenación por inserción directa y otra 
+para la comparación de dos fechas).
+*/
+
+#include <iostream> 
+using namespace std; 
+
+
+
+int main(){
+	
+	
+	return 0; 
+}
