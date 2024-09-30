d3832225216cfa2305acfa3caa237e4e564ba505
Guts <aamamanica@unjbg.edu.pe>
2024-09-29 23:36:49 -0500

    agregando

--- struOrd4.cpp
+++ struOrd4.cpp
@@ -20,8 +20,50 @@ struct Fecha{
 	int anho; 	
 };
 
+void llenaregistro(Fecha fe[], Personal per[], int n){
+	for(int i=0; i<n; i++){
+		cout<<"\nDNI: ";
+		cin.ignore(); 
+		getline(cin, per[i].dni);
+		cout<<"Nombre: ";
+		getline(cin, per[i].nombres); 
+		cout<<"Dia de nac: "; 
+		cin>>fe[i].dia; 
+		cout<<"Mes de nac: "; 
+		cin>>fe[i].mes; 
+		cout<<"Anho: "; 
+		cin>>fe[i].anho; 
+		cout<<"........................"; 
+		cout<<"\n"; 
+	}
+}
+
+
 int main(){
+	int n; 
 	
+	cout<<"Digite la cantidad de elementos de la estructura: "; 
+	cin>>n; 
+	
+	Fecha fe[n]; 
+	Personal per[n]; 
+	
+	int opcion; 
+	
+	do{
+		cout<<"1. Llenar registro. "<<endl; 
+		cout<<"2. Mostrar registro. "<<endl; 
+		cout<<"3. Mostrar ordenado. "<<endl; 
+		cout<<"Digite la opcion: "; 
+		cin>>opcion; 
+		
+		switch(opcion){
+			case 1:{
+				llenaregistro(fe, per, n); 
+				break;
+			}
+		}
+	}while(opcion != 0); 
 	
 	
 	return 0; 
