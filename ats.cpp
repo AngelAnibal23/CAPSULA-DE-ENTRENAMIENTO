a449d538592b390fdc9f6cf71a4d095eb36958d2
Guts <aamamanica@unjbg.edu.pe>
2024-10-30 20:39:09 -0500

    jaaaaa

---
+++ ats.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std; 
+
+class Persona{
+	private: 
+		int edad; 
+		string nombre; 
+	public: 
+		Persona(int, string); //constructor
+		void leer(); 
+		void correr(); 
+};
+
+int main(){
+	
+	
+	
+	return 0; 
+}
