62db268c44af5a7b418e656a1a138fec602a3d79
Guts <aamamanica@unjbg.edu.pe>
2024-10-31 10:16:00 -0500

    Terminando el ejercicio de programacion ATS

--- ats.cpp
+++ ats.cpp
@@ -4,18 +4,40 @@
 using namespace std; 
 
 class Persona{
-	private: 
+	private: // datos miembro
 		int edad; 
 		string nombre; 
-	public: 
+	public: // funciones miembro 
 		Persona(int, string); //constructor
 		void leer(); 
 		void correr(); 
 };
 
+//Constructor, nos sirve para inicializar los datos 
+Persona::Persona(int _edad, string _nombre){
+	edad = _edad; 
+	nombre = _nombre; 
+}
+
+void Persona::leer(){
+	cout<<"Soy "<<nombre<<" y estoy leyendo un libro y tengo "<<edad<<endl; 
+}
+
+void Persona::correr(){
+	cout<<"Soy "<<nombre<<" y estoy corriendo una maraton y tengo "<<edad<<endl; 
+}
+
 int main(){
+	Persona p1 = Persona(18, "Angel"); 
+	Persona p2(19, "Maria"); 
+	Persona p3(21, "Juan"); 
 	
-	
+	p1.leer();
+	 
+	p2.correr();
+	 
+	p3.leer(); 
+	p3.correr(); 
 	
 	return 0; 
 }
