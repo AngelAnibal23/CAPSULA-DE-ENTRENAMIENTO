0edb275583f04185a908f69dff30fc9b7d320791
Guts <aamamanica@unjbg.edu.pe>
2024-11-24 22:47:37 -0500

    ejercpy

---
+++ area.py
@@ -0,0 +1,14 @@
+'''
+Implementar un programa 
+'''
+
+import math  
+
+radio = float(input("Digite el radio: "))
+
+area = math.pi  * radio**2  
+
+longitud = 2 * math.pi * radio 
+
+print(f"El area es: {area:.2f}") 
+print(f"La longitud de la cirunferencia es: {longitud:.2f}") 
